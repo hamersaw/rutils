219a6f8eb5f8bb6be39b8ae2827ce3c4be7cdfd5
Daniel Rammer <hamersaw@bushpath.com>
2018-06-13 12:59:25 -0600

    modified the setup.sh script to build an okde-java.jar file and use that as a dependency instead of the gradle project.

--- setup.sh
+++ setup.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
+BASEDIR=$(dirname $0)
 
-mkdir libs
-git clone https://github.com/joluet/okde-java.git libs/okde-java
+# clone okde-java repository
+mkdir $BASEDIR/libs
+git clone https://github.com/joluet/okde-java.git $BASEDIR/libs/okde-java
+
+# build project 
+DIR=`pwd`
+cd $BASEDIR/libs/okde-java
+gradle build
+cd $DIR
+
+# copy jar file to libs
+cp $BASEDIR/libs/okde-java/build/libs/okde-java-classes.jar $BASEDIR/libs
+
+# remove git repository
+rm -rf $BASEDIR/libs/okde-java
