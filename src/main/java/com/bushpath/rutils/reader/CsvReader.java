72ee577c9a915171834495e52b40b250da7b5af4
Daniel Rammer <hamersaw@bushpath.com>
2018-07-22 18:12:36 -0600

    changed functionality of CsvReader. allows 'null' for 'features' so that all features are returned.

--- src/main/java/com/bushpath/rutils/reader/CsvReader.java
+++ src/main/java/com/bushpath/rutils/reader/CsvReader.java
@@ -6,37 +6,50 @@ import java.util.Collection;
 
 public class CsvReader extends Reader {
     protected BufferedReader in;
-    protected int[] indexes;
+    protected String[] features;
+    protected int[] indices;
 
-    public CsvReader(String file, Collection<String> features, String[] header) throws Exception {
+    public CsvReader(String file, Collection<String> features,
+            String[] header) throws Exception {
         // initialize BufferedReader
         this.in = new BufferedReader(new FileReader(file));
-
-        // initialize indexes
-        this.indexes = new int[features.size()];
 
         // parse header
         if (header == null || header.length == 0) {
             header = this.in.readLine().split(",");
         }
 
-        int featureIndex = 0;
-        for (String feature : features) {
-            // find index of feature in header
-            int index = -1;
-            for (int j=0; j<header.length; j++) {
-                if (feature.equals(header[j])) {
-                    index = j;
-                    break;
+        // initialize indices and features
+        if (features == null || features.isEmpty()) {
+            this.indices = new int[header.length];
+            for (int i=0; i<header.length; i++) {
+                this.indices[i] = i;
+            }
+
+            this.features = header;
+        } else {
+            this.indices = new int[features.size()];
+
+            int featureIndex = 0;
+            for (String feature : features) {
+                // find index of feature in header
+                int index = -1;
+                for (int j=0; j<header.length; j++) {
+                    if (feature.equals(header[j])) {
+                        index = j;
+                        break;
+                    }
                 }
+
+                if (index == -1) {
+                    throw new Exception("Feature '" + feature
+                        + "' not found in file '" + file + "'");
+                }
+
+                this.indices[featureIndex++] = index;
             }
 
-            if (index == -1) {
-                throw new Exception("Feature '" + feature
-                    + "' not found in file '" + file + "'");
-            }
-
-            this.indexes[featureIndex++] = index;
+            this.features = (String[]) features.toArray();
         }
     }
 
@@ -50,12 +63,16 @@ public class CsvReader extends Reader {
 
         // parse line
         String[] fields = line.split(",");
-        double[] record = new double[this.indexes.length];
-        for (int i=0; i<indexes.length; i++) {
-            record[i] = Double.parseDouble(fields[this.indexes[i]]);
+        double[] record = new double[this.indices.length];
+        for (int i=0; i<indices.length; i++) {
+            record[i] = Double.parseDouble(fields[this.indices[i]]);
         }
 
         return record;
+    }
+
+    public String[] getFeatures() {
+        return this.features;
     }
 
     @Override
