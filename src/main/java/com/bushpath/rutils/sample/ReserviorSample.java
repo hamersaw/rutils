f74dd1beb9d3e4243597d9a08ef8e30d859d063a
Daniel Rammer <hamersaw@bushpath.com>
2018-07-25 12:14:52 -0600

    added lock to reservior to make it thread safe.

--- src/main/java/com/bushpath/rutils/sample/ReserviorSample.java
+++ src/main/java/com/bushpath/rutils/sample/ReserviorSample.java
@@ -1,34 +1,48 @@
 package com.bushpath.rutils.sample;
 
 import java.util.Random;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class ReserviorSample {
     protected Random random;
     protected double[][] data;
     protected int count;
+    protected ReadWriteLock lock;
 
     public ReserviorSample(int count) {
         this.random = new Random(System.nanoTime());
         this.data = new double[count][];
         this.count = 0;
+        this.lock = new ReentrantReadWriteLock();
     }
 
     public void update(double[] d) throws Exception {
-        if (this.count < data.length) {
-            // if data is not full
-            this.data[this.count] = d;
-        } else {
-            // adopt value into data with probability data.length / this.count
-            if (random.nextDouble() <= (this.data.length / (this.count + 1))) {
-                // choose random item to replace
-                this.data[random.nextInt(this.data.length)] = d;
+        this.lock.writeLock().lock();
+        try {
+            if (this.count < data.length) {
+                // if data is not full
+                this.data[this.count] = d;
+            } else {
+                // adopt value into data with probability data.length / this.count
+                if (random.nextDouble() <= (this.data.length / (this.count + 1))) {
+                    // choose random item to replace
+                    this.data[random.nextInt(this.data.length)] = d;
+                }
             }
-        }
 
-        this.count += 1;
+            this.count += 1;
+        } finally {
+            this.lock.writeLock().unlock();
+        }
     }
 
     public double[][] getData() {
-        return this.data;
+        this.lock.readLock().lock();
+        try {
+            return this.data;
+        } finally {
+            this.lock.readLock().unlock();
+        }
     }
 }
